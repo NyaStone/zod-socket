b9589c1d2ed4f09f60450ec0e180b80cb5c98aa1
Pierre Lenertz <pierre@lenertz.fr>
2023-09-30 14:17:43 +0200

    typechecking acknowlegement callbacks on server

--- EventCategories.ts
+++ EventCategories.ts
@@ -1,17 +1,10 @@
 import {z, ZodType, ZodTypeAny} from "zod";
 
-export type ZodCollection = {
-    [key: string]: EventArguments;
-}
 
-type EventArguments = [...ZodTypeAny[], ZodTypeAny | [...ZodTypeAny[]]];
+export type LastInTuple<T extends any[]> = T extends [...args: any[], last: infer Last] ? Last : never;
+type DropLastInTuple<T extends any[]> = T extends [...args: infer U, rest: any[]] ? U : T;
 
-type LengthOfTuple<T extends any[]> = T extends { length: infer L } ? L : never;
-type DropFirstInTuple<T extends any[]> = T extends [arg: any, ...rest: infer U] ? U : T;
-type LastInTuple<T extends any[]> = T[LengthOfTuple<DropFirstInTuple<T>>];
-type DropLastInTuple<T extends any[]> = T extends [rest: infer U, ...args: any[]] ? U : T;
-
-type ExtractAckArgs<T extends EventArguments, AckArgs = LastInTuple<T>> = AckArgs extends ZodTypeAny[]
+export type ExtractAckArgs<T extends EventArguments, AckArgs = LastInTuple<T>> = AckArgs extends ZodTypeAny[]
     ? {
         [Index in keyof AckArgs]: AckArgs[Index] extends ZodTypeAny ? z.infer<AckArgs[Index]> : never
     } & {length: AckArgs['length']}
@@ -22,23 +15,31 @@ type ResolveZodTuple<T extends ZodTypeAny[]> = {
 
 type ResolveToCallback<T extends any[]> = (...args: T) => void;
 
-export type ResolveZodTypes<ZodTuple extends EventArguments,
-                            ExcludeLast = DropLastInTuple<ZodTuple>> = 
+export type ResolveEvents<SchemaMap extends ZodCollection> =  {[key in keyof SchemaMap]: ResolveZodTypes<SchemaMap[key]>}
+
+export type ResolveZodTypes<ZodTuple extends EventArguments> = 
 ZodTuple extends ZodTypeAny[]
     ? ResolveToCallback<ResolveZodTuple<ZodTuple>>
-    : ExcludeLast extends ZodTypeAny[] 
-        ? ResolveToCallback<[...ResolveZodTuple<ExcludeLast>, ResolveToCallback<ExtractAckArgs<ZodTuple>>]>
+    : DropLastInTuple<ZodTuple> extends ZodTypeAny[] 
+        ? ResolveToCallback<[...ResolveZodTuple<DropLastInTuple<ZodTuple>>, ResolveToCallback<ExtractAckArgs<ZodTuple>>]>
         : never
 
 
-export type DefaultCategories = {
-    clientToServer: {},
-    serverToClient: {},
-    // serverToServer: {}
+export type EventCategories<
+    CtoS extends ZodCollection = {},
+    StoC extends ZodCollection = {},
+    StoS extends ZodCollection = {}
+> = {
+    clientToServer: CtoS | {},
+    serverToClient: StoC | {},
+    serverToServer: StoS | {}
 }
 
-export type EventCategories = {
-    clientToServer: ZodCollection,
-    serverToClient: ZodCollection,
-    // serverToServer: ZodCollection
+export type ZodCollection = {
+    [key: string]: EventArguments;
 }
+
+export type ExtractZodCollection<Event extends Partial<EventCategories>, Name extends keyof EventCategories> =
+    Event[Name] extends ZodCollection ? Event[Name] : ZodCollection
+
+type EventArguments = [...ZodTypeAny[]] | [...ZodTypeAny[], [...ZodTypeAny[]]];
--- Server.ts
+++ Server.ts
@@ -1,42 +1,55 @@
 import {Server as ServerIO, ServerOptions} from "socket.io";
 import {Server as HttpServer} from "http";
 import {Server as HttpsServer} from "https";
-import { EventCategories, ResolveZodTypes, ZodCollection } from "./EventCategories";
-import { z } from "zod";
+import { EventCategories, ExtractAckArgs, ExtractZodCollection, ResolveEvents, ZodCollection } from "./EventCategories";
+import { ZodTypeAny } from "zod";
+import { EventNames, EventsMap } from "socket.io/dist/typed-events";
 
 export class Server< SocketData = {},
-        Events extends Partial<EventCategories> = {},
-        _ClientToServer extends ZodCollection = 
-            Events extends { clientToServer: ZodCollection } ? Events['clientToServer'] : ZodCollection,
-        _ServerToClient extends ZodCollection = 
-            Events extends { serverToClient: ZodCollection } ? Events['serverToClient'] : ZodCollection,
-        _ServerToServer extends ZodCollection = 
-            Events extends { serverToServer: ZodCollection } ? Events['serverToServer'] : ZodCollection
-        > 
+        Events extends Partial<EventCategories> = {}> 
     extends ServerIO<
-        {[key in keyof _ClientToServer]: ResolveZodTypes<_ClientToServer[key]>},
-        {[key in keyof _ServerToClient]: ResolveZodTypes<_ServerToClient[key]>},
-        {[key in keyof _ServerToServer]: ResolveZodTypes<_ServerToServer[key]>},
+        ResolveEvents<ExtractZodCollection<Events, 'clientToServer'>>,
+        ResolveEvents<ExtractZodCollection<Events, 'serverToClient'>>,
+        ResolveEvents<ExtractZodCollection<Events, 'serverToServer'>>,
         SocketData
     > 
     {
 
-    private events: Events;
+    private events: EventCategories<ExtractZodCollection<Events, 'clientToServer'>, ExtractZodCollection<Events, 'serverToClient'>, ExtractZodCollection<Events, 'serverToServer'>>;
     private _superEmit: typeof this.emit;
+
 
     constructor(events: Events, httpServer: HttpServer, opt?: Partial<ServerOptions>);
     constructor(events: Events, httpsServer: HttpsServer, opt?: Partial<ServerOptions>);
     constructor(events: Events, port: number, opt?: Partial<ServerOptions>);
     constructor(events: Events, opt?: Partial<ServerOptions>);
     constructor(arg1: Events, arg2?, arg3?) {
+        type ServerToClientEvents = ResolveEvents<ExtractZodCollection<Events, 'serverToClient'>>;
         super(arg2, arg3);
 
-        this.events = arg1;
+        this.events = {
+            clientToServer: arg1.clientToServer ? arg1.clientToServer : {},
+            serverToClient: arg1.serverToClient ? arg1.serverToClient : {},
+            serverToServer: arg1.serverToServer ? arg1.serverToServer : {}
 
-        // overriding the emit method to typecheck incoming acknowledgement
+        };
+
+        // overriding the emit method to wrap the acknowlegement callback with a typechecking method
         this._superEmit = this.emit;
-        this.emit = (...args) => {
-            return this._superEmit(...args);
+        this.emit = <Event extends EventNames<ServerToClientEvents>>(event: Event, ...args: Parameters<ServerToClientEvents[Event]>) => {
+                
+                const newArgs = args.map((arg) => {
+                    if (typeof arg !== 'function') return arg;
+                    return (...ackArgs: ExtractAckArgs<ExtractZodCollection<Events, 'serverToClient'>[Event]>) => {
+                        // typecheck the args
+                        for (let i = 0; i < ackArgs.length; i++) {
+                            const zodArg: ZodTypeAny = this.events.serverToClient[event.toString()][this.events.serverToClient[event.toString()].length - 1][i];
+                            zodArg.parse(ackArgs[i])
+                        }
+                        return arg(...ackArgs);
+                    }
+                }) as typeof args; // typecast nessecary, don't know why Array.map method doesn't pass types properly
+                return this._superEmit(event, ...newArgs);
         };
 
         // adding a middleware that will apply another middleware to all sockets
@@ -44,7 +57,7 @@ export class Server< SocketData = {},
             // adding a middleware to do type validation using the zod schema
             socket.use(([event, ...args], next) => {
                 
-                if (this.events.clientToServer && Object.keys(this.events.clientToServer).includes(event)) {
+                if (Object.keys(this.events.clientToServer).includes(event)) {
                     try {
                         for (let i = 0; i < this.events.clientToServer[event].length; i++) {
                             const zodArg = this.events.clientToServer[event][i];
