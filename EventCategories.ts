757449d2a817ea64576f313005acac07ac4ab84b
Pierre Lenertz <pierre@lenertz.fr>
2023-10-01 10:04:27 +0200

    Fixed ugly typecast and implement socket emit vali

--- EventCategories.ts
+++ EventCategories.ts
@@ -1,19 +1,19 @@
 import {z, ZodType, ZodTypeAny} from "zod";
 
 
-export type LastInTuple<T extends any[]> = T extends [...args: any[], last: infer Last] ? Last : never;
-type DropLastInTuple<T extends any[]> = T extends [...args: infer U, rest: any[]] ? U : T;
+export type LastInTuple<T extends EventArguments> = T extends [...args: any[], last: infer Last] ? Last : never;
+type DropLastInTuple<T extends EventArguments> = T extends [...args: infer U, rest: any[]] ? U : never;
 
-export type ExtractAckArgs<T extends EventArguments, AckArgs = LastInTuple<T>> = AckArgs extends ZodTypeAny[]
+export type ExtractAckArgs<T extends EventArguments> = LastInTuple<T> extends ZodTypeAny[]
     ? {
-        [Index in keyof AckArgs]: AckArgs[Index] extends ZodTypeAny ? z.infer<AckArgs[Index]> : never
-    } & {length: AckArgs['length']}
+        [Index in keyof LastInTuple<T>]: LastInTuple<T>[Index] extends ZodTypeAny ? z.infer<LastInTuple<T>[Index]> : never
+    } & {length: LastInTuple<T>['length']}
     : never
 type ResolveZodTuple<T extends ZodTypeAny[]> = {
     [Index in keyof T]: z.infer<T[Index]>;
 } & {length: T['length']}
 
-type ResolveToCallback<T extends any[]> = (...args: T) => void;
+export type ResolveToCallback<T extends any[]> = (...args: T) => void;
 
 export type ResolveEvents<SchemaMap extends ZodCollection> =  {[key in keyof SchemaMap]: ResolveZodTypes<SchemaMap[key]>}
 
@@ -30,9 +30,9 @@ export type EventCategories<
     StoC extends ZodCollection = {},
     StoS extends ZodCollection = {}
 > = {
-    clientToServer: CtoS | {},
-    serverToClient: StoC | {},
-    serverToServer: StoS | {}
+    clientToServer: CtoS,
+    serverToClient: StoC,
+    serverToServer: StoS
 }
 
 export type ZodCollection = {
@@ -43,3 +43,4 @@ export type ExtractZodCollection<Event extends Partial<EventCategories>, Name ex
     Event[Name] extends ZodCollection ? Event[Name] : ZodCollection
 
 type EventArguments = [...ZodTypeAny[]] | [...ZodTypeAny[], [...ZodTypeAny[]]];
+
--- Server.ts
+++ Server.ts
@@ -1,7 +1,7 @@
 import {Server as ServerIO, ServerOptions} from "socket.io";
 import {Server as HttpServer} from "http";
 import {Server as HttpsServer} from "https";
-import { EventCategories, ExtractAckArgs, ExtractZodCollection, ResolveEvents, ZodCollection } from "./EventCategories";
+import { EventCategories, ExtractAckArgs, ExtractZodCollection, ResolveEvents, ResolveToCallback, ZodCollection } from "./EventCategories";
 import { ZodTypeAny } from "zod";
 import { EventNames, EventsMap } from "socket.io/dist/typed-events";
 
@@ -31,16 +31,38 @@ export class Server< SocketData = {},
             clientToServer: arg1.clientToServer ? arg1.clientToServer : {},
             serverToClient: arg1.serverToClient ? arg1.serverToClient : {},
             serverToServer: arg1.serverToServer ? arg1.serverToServer : {}
-
         };
 
         // overriding the emit method to wrap the acknowlegement callback with a typechecking method
         this._superEmit = this.emit;
-        this.emit = <Event extends EventNames<ServerToClientEvents>>(event: Event, ...args: Parameters<ServerToClientEvents[Event]>) => {
-                
-                const newArgs = args.map((arg) => {
-                    if (typeof arg !== 'function') return arg;
-                    return (...ackArgs: ExtractAckArgs<ExtractZodCollection<Events, 'serverToClient'>[Event]>) => {
+        this.emit = <Event extends EventNames<ServerToClientEvents>>(event: Event, ...args: Parameters<ServerToClientEvents[Event]>) => {      
+            const newArgs = [...args] as const;
+            
+            if (typeof newArgs[newArgs.length - 1] === 'function') {
+                const arg = newArgs[newArgs.length - 1]
+                newArgs[newArgs.length - 1] = (...ackArgs) => {
+                    // typecheck the args
+                    for (let i = 0; i < ackArgs.length; i++) {
+                        const zodArg: ZodTypeAny = this.events.serverToClient[event.toString()][this.events.serverToClient[event.toString()].length - 1][i];
+                        zodArg.parse(ackArgs[i])
+                    }
+                    return arg(...ackArgs);
+                }
+            }
+            
+            return this._superEmit(event, ...newArgs);
+        };
+
+        // adding a middleware that will apply modifications to extend sockets
+        this.use((socket, next) => {
+            // extending the emit method to account for acknowlegement callback type validation 
+            const superEmit = socket.emit;
+            socket.emit = <Event extends EventNames<ServerToClientEvents>>(event: Event, ...args: Parameters<ServerToClientEvents[Event]>) => {
+                const newArgs = [...args] as const;
+            
+                if (typeof newArgs[newArgs.length - 1] === 'function') {
+                    const arg = newArgs[newArgs.length - 1]
+                    newArgs[newArgs.length - 1] = (...ackArgs) => {
                         // typecheck the args
                         for (let i = 0; i < ackArgs.length; i++) {
                             const zodArg: ZodTypeAny = this.events.serverToClient[event.toString()][this.events.serverToClient[event.toString()].length - 1][i];
@@ -48,12 +70,10 @@ export class Server< SocketData = {},
                         }
                         return arg(...ackArgs);
                     }
-                }) as typeof args; // typecast nessecary, don't know why Array.map method doesn't pass types properly
-                return this._superEmit(event, ...newArgs);
-        };
-
-        // adding a middleware that will apply another middleware to all sockets
-        this.use((socket, next) => {
+                }
+                
+                return superEmit(event, ...newArgs);
+            }
             // adding a middleware to do type validation using the zod schema
             socket.use(([event, ...args], next) => {
                 
